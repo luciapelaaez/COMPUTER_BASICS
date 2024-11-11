84e43c5aa3b3a87e4eba2c1275fea9e7afba8cf3
luciapelaaez <lpeladel@myuax.com>
2024-11-11 22:41:36 +0100

    Delete Conversion of Temperature.py

--- Conversion of Temperature.py
+++
@@ -1,9 +0,0 @@
-print ("I'm going to convert a temperature in degrees Celsius to degrees Fahrenheit.")
-celsius = float(input ("Please give me a temperature in degrees Celsius."))
-print ("The temperature you gave me is:", celsius)
-def conversion(celsius):
-    resultado = celsius*9/5+32
-    return resultado
-
-print ("The temperature that you gave me in degrees Celsius, in degrees Fahrenheit is:", conversion (celsius))
-
